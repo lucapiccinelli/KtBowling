4c7848dac3d4d7efc91a72d7d1d3f287198bc78e
Luca Piccinelli <luca.piccinelli@cgm.com>
2019-03-25 18:00:06 +0100

    second refactory step

--- src/main/kotlin/com/g3/ktbowling/Program.kt
+++ src/main/kotlin/com/g3/ktbowling/Program.kt
@@ -9,35 +9,23 @@ fun main(args: Array<String>) {
 }
 
 
-private fun playTheGame(inputRolls: Queue<Int>): Int {
+private fun playTheGame(rolls: Queue<Roll>): Int {
     var total: Int = 0
     var frameCount = 0
-    while (!inputRolls.isEmpty() && frameCount < 10) {
-        val firstRoll = inputRolls.remove()
 
-        if (!canTake(1, inputRolls)) {
+    while (!rolls.isEmpty() && frameCount < 10) {
+        val firstRoll = rolls.remove()
+
+        if (!canTake(1, rolls)) {
             return -1
         }
 
         var frameValue = 0
-        if (firstRoll == 10) {
+        val (newTotal, newFrameValue) = computeFrameValue(firstRoll, rolls, total)
+        total = newTotal
+        frameValue = newFrameValue
 
-            val (newTotal, newFrameValue) = assignBonus(inputRolls, firstRoll, 2, total)
-            total = newTotal
-            frameValue = newFrameValue
-
-        } else {
-
-            frameValue = firstRoll + inputRolls.remove()
-
-            if (frameValue == 10) {
-                val (newTotal, newFrameValue) = assignBonus(inputRolls, frameValue, 1, total)
-                total = newTotal
-                frameValue = newFrameValue
-            }
-
-        }
-        if (total == -1) break
+        if (total == -1) return -1
 
         frameCount++
         total += frameValue
@@ -46,24 +34,44 @@ private fun playTheGame(inputRolls: Queue<Int>): Int {
     return total
 }
 
+private fun computeFrameValue(firstRoll: Roll, rolls: Queue<Roll>, runningTotal: Int) : Pair<Int, Int>{
+    if (firstRoll.rollValue == 10) {
+        return assignBonus(rolls, firstRoll.rollValue, 2, runningTotal)
+    } else {
+        val frameValue = firstRoll.rollValue + rolls.remove().rollValue
+        if (frameValue == 10) {
+            return assignBonus(rolls, frameValue, 1, runningTotal)
+        }
+        return Pair(runningTotal, frameValue)
+    }
+}
+
 private operator fun <E> Queue<E>.get(i: Int): E {
     return this.elementAt(i)
 }
 
-fun canTake(howMany: Int, fromCollection: Collection<Int>): Boolean = fromCollection.size >= howMany
+fun canTake(howMany: Int, fromCollection: Collection<Roll>): Boolean = fromCollection.size >= howMany
 
-fun assignBonus(rolls: Collection<Int>, partialValue: Int, bonusRolls: Int, totalIn: Int): Pair<Int,Int> {
+fun assignBonus(rolls: Collection<Roll>, partialValue: Int, bonusRolls: Int, totalIn: Int): Pair<Int,Int> {
     if (!canTake(bonusRolls, rolls)) {
         return Pair(-1, partialValue)
     }
     return Pair(totalIn, partialValue + computeBonus(rolls, bonusRolls))
 }
 
-private fun computeBonus(inputRolls: Collection<Int>, bonus: Int) =
-        inputRolls.take(bonus).sum()
+private fun computeBonus(inputRolls: Collection<Roll>, bonus: Int) =
+        inputRolls
+                .take(bonus)
+                .sumBy { it.rollValue }
 
 private fun printTheOutput(total: Int) {
     println(if (total > 0) total else "")
 }
 
-private fun readInput(args: Array<String>) = args[0].split(",").map(String::toInt)
+private fun readInput(args: Array<String>) = args[0].split(",")
+        .map(String::toInt)
+        .map { Roll(it) }
+
+
+class Roll(val rollValue: Int) {
+}
